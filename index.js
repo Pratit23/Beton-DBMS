aa6e35f5fea51e603681ee757f55c1bee4d5ec07
Pratit23 <51255935+Pratit23@users.noreply.github.com>
2021-02-25 20:01:24 +0530

    production code changes

--- index.js
+++ index.js
@@ -10,35 +10,43 @@ const { URI } = require('./keys/dev')
 const app = express();
 
 app.use(cors());
-app.use( bodyParser.json());
+app.use(bodyParser.json());
 
 // having options to connect to the db
 options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify:false
+    useFindAndModify: false
 }
 
 // connecting to the db
 mongoose.connect(URI, options);
 
 // adding event listeners
-mongoose.connection.once('connected', ()=>{
+mongoose.connection.once('connected', () => {
     console.log("Connected to the Database!");
-}).on('error', ()=>{
+}).on('error', () => {
     console.log("Error connecting to the Database!");
 });
 
 
 // graphql route
-app.use('/graphql',graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
 
 
 // listening on port
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static('Beton/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'Beton', 'build', 'index.html'))
+    })
+}
 PORT = process.env.PORT || 1000;
-const server = app.listen(PORT, ()=>{
+const server = app.listen(PORT, () => {
     console.log("Listening to requests on port", PORT);
 });
+
