6e89b1ed0d86a8c7a111995ecfee2b07f90766cb
aajinkya1203 <aajinkya1203@gmail.com>
2021-02-27 20:21:18 +0530

    Logical tings

--- Beton/src/components/Layouts/Advertiser/AdvHomepage.js
+++ Beton/src/components/Layouts/Advertiser/AdvHomepage.js
@@ -2,7 +2,13 @@ import React from 'react'
 import AdvertiserStatsCard from '../../Cards/AdvertiserStatsCard'
 import AdvOne from '../../../images/advOne.png'
 import AdvTwo from '../../../images/advTwo.png'
-import AdvThree from '../../../images/advThree.png'
+import AdvThree from '../../../images/AdvThree.png'
+import AdvChart from '../../Charts/AdvChart'
+import AdvDonut from '../../Charts/AdvDonut'
+import MinimalImageCard from '../../Cards/MinimalImageCard'
+import One from '../../../images/one.png'
+import Two from '../../../images/two.png'
+
 
 const AdvHomepage = () => {
     return (
@@ -14,6 +20,25 @@ const AdvHomepage = () => {
                     <AdvertiserStatsCard text="Advertisments" src={AdvOne} stat="05" />
                     <AdvertiserStatsCard text="Total Coupons" src={AdvTwo} stat="03" />
                     <AdvertiserStatsCard text="Available Coupons" src={AdvThree} stat="01" />
+                    <div className="col s12 advChart">
+                        <AdvChart />
+                    </div>
+                </div>
+                <div className="col s12 m4 advStatsInfographics">
+                    <h3 style={{ margin: "30px 0 0 30px" }}>Statistics</h3>
+                    <hr className="divider" />
+                    <div className="col s12">
+                        <AdvDonut />
+                    </div>
+                    <h3 className="col s12" style={{ margin: "20px", marginBottom: "0" }}>
+                        Quick links
+                    </h3>
+                    <div className="col s12" style={{borderRadius: "18px", height: "100px", margin: "10px 0"}} >
+                        <MinimalImageCard img={One} text="Map view" link="/cluster" />
+                    </div>
+                    <div className="col s12" style={{borderRadius: "18px", height: "100px", margin: "10px 0"}} >
+                        <MinimalImageCard img={Two} text="My account" link="/account" />
+                    </div>
                 </div>
             </div>
         </div>
