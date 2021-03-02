21a8ddaaed7c0e427571c066396d23465f1cd591
aajinkya1203 <aajinkya1203@gmail.com>
2021-03-03 00:24:52 +0530

    Mostly all advertiser frontend done

--- Beton/src/components/Layouts/Advertiser/AddAdvertisment.js
+++ Beton/src/components/Layouts/Advertiser/AddAdvertisment.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import AdvSidenav from './AdvSidenav';
 import AddAdvCover from '../../../images/AddAdvCover.png';
+import GeneralInput from '../../Buttons/GeneralInput';
+import URL from '../../Buttons/URL';
 
 const AddAdvertisment = () => {
 
@@ -17,11 +19,56 @@ const AddAdvertisment = () => {
                     <div className="col s12 m4 l6" style={{
                         backgroundImage: `url(${AddAdvCover})`,
                         height: "100%",
-                    }} ></div>
+                    }} >
+                    </div>
                     <div className="col s12 m8 l6">
                         <div className="col s12">
                             <h3 style={{ margin: "30px 0 0 30px" }}>Add an advertisment</h3>
                             <hr className="divider" />
+                        </div>
+
+                        {/* instruction box here */}
+                        <div className="col s12 valign-wrapper hide-on-med-and-down" style={{
+                            backgroundImage: `url(${instructionCover})`,
+                            height: "150px",
+                            borderRadius: "20px"
+                        }} >
+                            <p className="white-text col s4 center-align" style={{
+                                display: "inline-block",
+                                whiteSpace: "nowrap"
+                            }} >
+                                1. Add CSV <br /><a href={FormatCover} target="__blank">(Check format)</a>
+                            </p>
+                            
+                            <p className="white-text col s4 center-align" style={{
+                                display: "inline-block",
+                                whiteSpace: "nowrap"
+                            }} >
+                                2. All pass?
+                            </p>
+                            <p className="white-text col s4 center-align" style={{
+                                display: "inline-block",
+                                whiteSpace: "nowrap"
+                            }} >
+                                3. Submit
+                            </p>
+                        </div>
+
+                        {/* title */}
+                        <GeneralInput placeholder="Catchy line for your ad" classy="col s10 offset-s1 m9 offset-m2 l6 offset-l3" type="text" id="add-title" />
+
+                        {/* link */}
+                        <URL classy="col s10 offset-s1 m9 offset-m2 l6 offset-l3"/>
+
+                        {/* image */}
+                        <div className="file-field input-field col s10 offset-s1 m9 offset-m2 l6 offset-l3">
+                            <div className="btn">
+                                <span>File</span>
+                                <input type="file" />
+                            </div>
+                            <div className="file-path-wrapper">
+                                <input className="file-path validate" type="text" />
+                            </div>
                         </div>
 
                         <div className="col s12" style={{ textAlign: "center", marginTop: "20px" }}>
