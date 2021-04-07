44a60476a5d9c3bc44bd2a6bfb079f9853ae4b4e
Pratit23 <51255935+Pratit23@users.noreply.github.com>
2021-04-07 18:42:50 +0530

    minor change

--- Beton/src/App.js
+++ Beton/src/App.js
@@ -40,7 +40,7 @@ import SpecificTender from './components/Layouts/Admin/SpecificTender';
 
 // setting up an apollo client
 const client = new ApolloClient({
-  uri: 'http://localhost:1000/graphql',
+  uri: '/graphql',
   onError: ({ response, operation, graphQLErrors, networkError }) => {
     if (operation.operationName === "IgnoreErrorsQuery") {
       response.errors = null;
